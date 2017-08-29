83d8ade6f5df50dba0d27e516789846a54f576f2
Ozols <its27eo1@odin.local>
2017-08-29 11:08:48 +0300

    List comprehension

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,14 +1,26 @@
 defmodule Cards do
 
   def create_deck do
-    values = ["Ace", "Two", "Three", "Four", "Five"]
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-    for value <- values do
-      for suit <- suits do
+
+    # Nested comperhantions
+    # Get all lists into one list
+    # Solution 1 - Flattens the given list of nested lists, many lists moved into one list
+
+    #cards = for value <- values do
+    #  for suit <- suits do
+    #    "#{value} of #{suit}"
+    #  end
+    #end        
+    #List.flatten(cards)
+
+      # Solution 2 - Passing both lists on the same comprehension 
+
+      for suit <- suits, value <- values do
         "#{value} of #{suit}"
       end
-    end    
   end
 
   def shuffle(deck) do
